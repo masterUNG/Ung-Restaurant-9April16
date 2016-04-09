0e744f4bc2664a1c836adf1bb35b8e39849bdfb6
masterUNG <phrombutr@gmail.com>
2016-04-09 11:58:01 +0700

    Add & Delete Finish

--- app/src/main/java/appewtc/masterung/ungrestautant/MainActivity.java
+++ app/src/main/java/appewtc/masterung/ungrestautant/MainActivity.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.ungrestautant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -16,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         //Request SQLite
         myManage = new MyManage(this);
 
+        //Test Add Value
+        //testAdd();
+
+        //Delete SQLite
+        deleteSQLite();
+
 
     }   // Main Method
+
+    private void deleteSQLite() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.food_table, null, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+    }
+
+    private void testAdd() {
+        myManage.addValueToSQLite(0, "user", "pass", "name");
+        myManage.addValueToSQLite(1, "food", "price", "source");
+    }
 
 }   // Main Class
--- app/src/main/java/appewtc/masterung/ungrestautant/MyManage.java
+++ app/src/main/java/appewtc/masterung/ungrestautant/MyManage.java
@@ -1,5 +1,6 @@
 package appewtc.masterung.ungrestautant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
@@ -12,11 +13,52 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static final String user_table = "userTABLE";
+    public static final String column_id = "_id";
+    public static final String column_user = "User";
+    public static final String column_pass = "Password";
+    public static final String column_name = "Name";
+
+    public static final String food_table = "foodTABLE";
+    public static final String column_food = "Food";
+    public static final String column_price = "Price";
+    public static final String column_source = "Source";
+
     public MyManage(Context context) {
 
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
 
     }   // Constructor
+
+    public long addValueToSQLite(int intTABLE,
+                                 String strFirst,
+                                 String strSecond,
+                                 String strThird) {
+
+        ContentValues contentValues = new ContentValues();
+        long myLong = 0;
+        switch (intTABLE) {
+
+            case 0:
+                // for userTABLE
+                contentValues.put(column_user, strFirst);
+                contentValues.put(column_pass, strSecond);
+                contentValues.put(column_name, strThird);
+
+                myLong = sqLiteDatabase.insert(user_table, null, contentValues);
+                break;
+            case 1:
+                // for foodTABLE
+                contentValues.put(column_food, strFirst);
+                contentValues.put(column_price, strSecond);
+                contentValues.put(column_source, strThird);
+
+                myLong = sqLiteDatabase.insert(food_table, null, contentValues);
+                break;
+        }   // switch
+
+        return myLong;
+    }
 
 }   // Main Class
